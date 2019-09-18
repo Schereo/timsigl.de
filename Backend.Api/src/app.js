cae12771cb0ea53c5497e1abd901910913022567
Tim Sigl <tim.sigl@hotmail.de>
2019-09-18 16:56:56 +0200

    setup for heroku

--- Backend.Api/src/app.js
+++ Backend.Api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('', cors(), (req, res) => {
     res.send({
@@ -10,4 +11,4 @@ app.get('', cors(), (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(port, () => console.log('Server is running on port' + port));
