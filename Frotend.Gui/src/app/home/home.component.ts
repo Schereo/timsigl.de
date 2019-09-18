3a148c83a97bb5bb514e99346016ce5a3f208d0d
Tim Sigl <tim.sigl@hotmail.de>
2019-09-18 17:18:53 +0200

    added environemt variables

--- Frotend.Gui/src/app/home/home.component.ts
+++ Frotend.Gui/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { User } from 'src/models/user';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -20,7 +21,7 @@ export class HomeComponent implements OnInit {
   getData(){
     var params = new HttpParams();
     params.set('search', '3dprinting');
-    this.http.get('http://localhost:3000').subscribe((ans) => console.log('Answer', ans));
+    this.http.get(environment.apiUrl).subscribe((ans) => console.log('Answer', ans));
   }
 
 }
