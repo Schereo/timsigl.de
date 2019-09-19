27f2f13c3202557b5c0532196d0c29c3c6abfeea
Tim Sigl <tim.sigl@hotmail.de>
2019-09-19 13:33:53 +0200

    completed user rest api and updated (now) index.js to sue new models

--- Backend.Api/src/index.js
+++ Backend.Api/src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('./db/mongodb');
+const userRouter = require('./router/user');
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.get('', cors(), (req, res) => {
-    res.send({
-        name: 'Tim',
-        age: 22
-    });
-});
+app.use(cors({
+    origin: 'http://timsigl.de'
+}));
+app.use(express.json());
+app.use(userRouter);
 
 app.listen(port, () => console.log('Server is running on port ' + port));
