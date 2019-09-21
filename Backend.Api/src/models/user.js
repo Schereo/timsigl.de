65b8e9fda59dd1f761707793555bae186d950e2a
Tim Sigl <tim.sigl@hotmail.de>
2019-09-21 22:49:55 +0200

    Added authenticcation middleware, added endpoint for own profile, added jwts

--- Backend.Api/src/models/user.js
+++ Backend.Api/src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         trim: true,
@@ -27,7 +29,52 @@ const User = mongoose.model('User', {
     mailVerified: {
         type: Boolean,
         default: false
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.methods.generateAuthToken = async function (email, password) {
+  const token = jwt.sign({_id: this._id.toString()}, 'timspersonalwebsite2019');
+
+  this.tokens = this.tokens.concat({token});
+  await this.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({
+        email
+    });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+};
+
+// Hash password when creating or updating account
+userSchema.pre('save', async function (next) {
+    
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- Backend.Api/src/router/user.js
+++ Backend.Api/src/router/user.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
+   res.send(req.user);
+});
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({
+            user,
+            token
+        });
+    } catch (e) {
+        res.status(400).send();
+    }
+});
+
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({});
         res.send(users);
@@ -38,9 +57,15 @@ router.get('/users/:id', async (req, res) => {
 });
 
 router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true});
+        const user  = await User.findById(req.params.id);
+        updates.forEach((update) => {
+            user[update] = req.body[update];
+        });
+
+        await user.save();
 
         if (!user) {
             return res.status(404).send();
@@ -58,12 +83,12 @@ router.delete('/users/:id', async (req, res) => {
         const user = await User.findByIdAndDelete(req.params.id);
 
         if (!user) {
-            return res.status(404).send()
+            return res.status(404).send();
         }
 
         res.send(user);
     } catch (e) {
-        res.status(500).send()
+        res.status(500).send();
     }
 });
 
